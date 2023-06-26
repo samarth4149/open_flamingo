436326675991af1f46b6de190615d6eb3cadb338
Ximeng Sun <sunxm@crc-dot1x-nat-10-239-16-96.bu.edu>
2023-06-26 15:08:42 -0400

    add a caption eval

--- sample_eval.py
+++ sample_eval.py
@@ -19,76 +19,13 @@ model.load_state_dict(torch.load(checkpoint_path), strict=False)
 from PIL import Image
 import requests
 
-# """
-# Step 1: Load images
-# """
-#
-# query_image = Image.open(
-#     requests.get(
-#         "http://images.cocodataset.org/test-stuff2017/000000028137.jp",
-#         stream=True
-#     ).raw
-# )
-#
-#
-# """
-# Step 2: Preprocessing images
-# Details: For OpenFlamingo, we expect the image to be a torch tensor of shape
-#  batch_size x num_media x num_frames x channels x height x width.
-#  In this case batch_size = 1, num_media = 3, num_frames = 1
-#  (this will always be one expect for video which we don't support yet),
-#  channels = 3, height = 224, width = 224.
-# """
-# vision_x = [image_processor(query_image).unsqueeze(0)]
-# vision_x = torch.cat(vision_x, dim=0)
-# vision_x = vision_x.unsqueeze(1).unsqueeze(0)
-#
-# """
-# Step 3: Preprocessing text
-# Details: In the text we expect an <image> special token to indicate where an image is.
-#  We also expect an <|endofchunk|> special token to indicate the end of the text
-#  portion associated with an image.
-# """
-# tokenizer.padding_side = "left" # For generation padding tokens should be on the left
-# lang_x = tokenizer(
-#     ["<image>caption:"],
-#     return_tensors="pt",
-# )
-#
-#
-# """
-# Step 4: Generate text
-# """
-# generated_text = model.generate(
-#     vision_x=vision_x,
-#     lang_x=lang_x["input_ids"],
-#     attention_mask=lang_x["attention_mask"],
-#     max_new_tokens=20,
-#     num_beams=3,
-# )
-#
-# print("Generated text: ", tokenizer.decode(generated_text[0]))
-
-
 """
 Step 1: Load images
 """
-demo_image_one = Image.open(
-    requests.get(
-        "http://images.cocodataset.org/val2017/000000039769.jpg", stream=True
-    ).raw
-)
-
-# demo_image_two = Image.open(
-#     requests.get(
-#         "http://images.cocodataset.org/test-stuff2017/000000028137.jpg",
-#         stream=True
-#     ).raw
-# )
 
 query_image = Image.open(
     requests.get(
-        "http://images.cocodataset.org/test-stuff2017/000000028352.jpg",
+        "http://images.cocodataset.org/test-stuff2017/000000028137.jpg",
         stream=True
     ).raw
 )
@@ -102,7 +39,7 @@ Details: For OpenFlamingo, we expect the image to be a torch tensor of shape
  (this will always be one expect for video which we don't support yet),
  channels = 3, height = 224, width = 224.
 """
-vision_x = [image_processor(demo_image_one).unsqueeze(0), image_processor(query_image).unsqueeze(0)]
+vision_x = [image_processor(query_image).unsqueeze(0)]
 vision_x = torch.cat(vision_x, dim=0)
 vision_x = vision_x.unsqueeze(1).unsqueeze(0)
 
@@ -114,7 +51,7 @@ Details: In the text we expect an <image> special token to indicate where an ima
 """
 tokenizer.padding_side = "left" # For generation padding tokens should be on the left
 lang_x = tokenizer(
-    ["<image>caption: cats asleep on the coach.<|endofchunk|><image>caption:"],
+    ["<image>caption:"],
     return_tensors="pt",
 )
 
@@ -131,3 +68,66 @@ generated_text = model.generate(
 )
 
 print("Generated text: ", tokenizer.decode(generated_text[0]))
+
+#
+# """
+# Step 1: Load images
+# """
+# demo_image_one = Image.open(
+#     requests.get(
+#         "http://images.cocodataset.org/val2017/000000039769.jpg", stream=True
+#     ).raw
+# )
+#
+# # demo_image_two = Image.open(
+# #     requests.get(
+# #         "http://images.cocodataset.org/test-stuff2017/000000028137.jpg",
+# #         stream=True
+# #     ).raw
+# # )
+#
+# query_image = Image.open(
+#     requests.get(
+#         "http://images.cocodataset.org/test-stuff2017/000000028352.jpg",
+#         stream=True
+#     ).raw
+# )
+#
+#
+# """
+# Step 2: Preprocessing images
+# Details: For OpenFlamingo, we expect the image to be a torch tensor of shape
+#  batch_size x num_media x num_frames x channels x height x width.
+#  In this case batch_size = 1, num_media = 3, num_frames = 1
+#  (this will always be one expect for video which we don't support yet),
+#  channels = 3, height = 224, width = 224.
+# """
+# vision_x = [image_processor(demo_image_one).unsqueeze(0), image_processor(query_image).unsqueeze(0)]
+# vision_x = torch.cat(vision_x, dim=0)
+# vision_x = vision_x.unsqueeze(1).unsqueeze(0)
+#
+# """
+# Step 3: Preprocessing text
+# Details: In the text we expect an <image> special token to indicate where an image is.
+#  We also expect an <|endofchunk|> special token to indicate the end of the text
+#  portion associated with an image.
+# """
+# tokenizer.padding_side = "left" # For generation padding tokens should be on the left
+# lang_x = tokenizer(
+#     ["<image>caption: cats asleep on the coach.<|endofchunk|><image>caption:"],
+#     return_tensors="pt",
+# )
+#
+#
+# """
+# Step 4: Generate text
+# """
+# generated_text = model.generate(
+#     vision_x=vision_x,
+#     lang_x=lang_x["input_ids"],
+#     attention_mask=lang_x["attention_mask"],
+#     max_new_tokens=20,
+#     num_beams=3,
+# )
+#
+# print("Generated text: ", tokenizer.decode(generated_text[0]))
