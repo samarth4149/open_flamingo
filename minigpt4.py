581977d4a5975b164f6d96bb8c715a75e9240f3a
Ximeng Sun <sunxm@Ximengs-Air.attlocal.net>
2023-07-10 15:39:42 -0700

    add minigpt4

--- minigpt4.py
+++ minigpt4.py
@@ -37,6 +37,77 @@ class StoppingCriteriaSub(StoppingCriteria):
 
         return False
 
+
+class MiniGPT4():
+    def __init__(self, model_config, gpu_id):
+        self.model_config = model_config
+        self.model_config.device_8bit = gpu_id
+        model_cls = registry.get_model_class(model_config.arch)
+        self.model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
+
+        vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
+        self.vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
+
+        stop_words_ids = [torch.tensor([835]).to('cuda:{}'.format(args.gpu_id)),
+                          torch.tensor([2277, 29937]).to('cuda:{}'.format(args.gpu_id))]
+        self.stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
+        self.device = 'cuda:{}'.format(args.gpu_id)
+
+    @staticmethod
+    def create_prompt(system, sep, messages):
+        prompt = system + sep
+        for role, message in messages:
+            if message:
+                prompt += role + ": " + message + sep
+            else:
+                prompt += role + ":"
+        return prompt
+
+    @staticmethod
+    def expand_emb(emb, batch_size):
+        return emb.tile((batch_size, 1))
+
+    def get_outputs(self, batch_images,
+                    system = "Give the following image: <Img>ImageContent</Img>. You will be able to see the image once I provide it to you. Please answer my questions.",
+                    sep = "###", prompt = 'describe the image as detailed as possible',
+                    max_new_tokens=200, num_beams=1, do_sample=True, min_length=1, top_p=0.9, repetition_penalty=1.0,
+                    length_penalty=1, temperature=1.0):
+        batch_images = batch_images.to(self.device)
+        image_emb, _ = self.model.encode_img(batch_images)
+
+        messages= [("Human", "<Img><ImageHere></Img> %s" % prompt), ("Assistant", None)]
+        sentence = self.create_prompt(system, sep, messages)
+
+        sentence_segs = sentence.split('<ImageHere>')
+
+        seg_tokens = [
+            self.model.llama_tokenizer(
+                seg, return_tensors="pt", add_special_tokens=i == 0).to('cuda:{}'.format(args.gpu_id)).input_ids
+            # only add bos to the first seg
+            for i, seg in enumerate(sentence_segs)
+        ]
+
+        seg_embs = [self.model.llama_model.model.embed_tokens(seg_t) for seg_t in seg_tokens]
+        embs = [self.expand_emb(seg_embs[0], batch_images.shape[0]), image_emb, self.expand_emb(seg_embs[1], batch_images.shape[0])]
+        mixed_embs = torch.cat(embs, dim=1)
+
+
+        outputs = self.model.llama_model.generate(
+            inputs_embeds=mixed_embs,
+            max_new_tokens=max_new_tokens,
+            stopping_criteria=self.stopping_criteria,
+            num_beams=num_beams,
+            do_sample=do_sample,
+            min_length=min_length,
+            top_p=top_p,
+            repetition_penalty=repetition_penalty,
+            length_penalty=length_penalty,
+            temperature=temperature
+        )
+
+        return outputs
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Demo")
     parser.add_argument("--cfg-path", required=True, help="path to configuration file.")
@@ -56,79 +127,20 @@ def parse_args():
 args = parse_args()
 cfg = Config(args)
 
+model = MiniGPT4(cfg.model_cfg, args.gpu_id)
+vis_processor = model.vis_processor
 
-model_config = cfg.model_cfg
-model_config.device_8bit = args.gpu_id
-model_cls = registry.get_model_class(model_config.arch)
-model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
-
-vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
-vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
-
-stop_words_ids = [torch.tensor([835]).to('cuda:{}'.format(args.gpu_id)),
-                          torch.tensor([2277, 29937]).to('cuda:{}'.format(args.gpu_id))]
-stopping_criteria = StoppingCriteriaList([StoppingCriteriaSub(stops=stop_words_ids)])
 
 img_path = "/projectnb/ivc-ml/sunxm/code/MiniGPT-4/test_examples/test1.png"
 
 raw_image = Image.open(img_path).convert('RGB')
 image = vis_processor(raw_image).unsqueeze(0).to('cuda:{}'.format(args.gpu_id))
 
-# prepare the input to llama
-system = "Give the following image: <Img>ImageContent</Img>. You will be able to see the image once I provide it to you. Please answer my questions."
-roles = ("Human", "Assistant")
-messages = []
-offset = 2
-sep = "###"
+outputs = model.get_outputs(batch_images=image, )
 
-image_emb, _ = model.encode_img(image)
-image_list = [image_emb]
-messages.append(("Human", "<Img><ImageHere></Img>"))
-messages[0] = ("Human", "<Img><ImageHere></Img> %s" % 'describe the image as detailed as possible' )
-messages.append(["Assistant", None])
+new_predicitions = [process_output(output) for output in outputs]
 
-
-prompt = system + sep
-for role, message in messages:
-    if message:
-        prompt += role + ": " + message + sep
-    else:
-        prompt += role + ":"
-
-prompt_segs = prompt.split('<ImageHere>')
-
-seg_tokens = [
-            model.llama_tokenizer(
-                seg, return_tensors="pt", add_special_tokens=i == 0).to('cuda:{}'.format(args.gpu_id)).input_ids
-            # only add bos to the first seg
-            for i, seg in enumerate(prompt_segs)
-        ]
-
-seg_embs = [model.llama_model.model.embed_tokens(seg_t) for seg_t in seg_tokens]
-import pdb
-pdb.set_trace()
-embs = [seg_embs[0],  image_emb,  seg_embs[1]]
-mixed_embs = torch.cat(embs, dim=1)
-
-current_max_len = embs.shape[1] + 300
-
-outputs = model.llama_model.generate(
-            inputs_embeds=embs,
-            max_new_tokens=200,
-            stopping_criteria=stopping_criteria,
-            num_beams=1,
-            do_sample=True,
-            min_length=1,
-            top_p=0.9,
-            repetition_penalty=1.0,
-            length_penalty=1,
-            temperature=1.0,
-        )
-
-# take the first example in the batch
-output_token = outputs[0]
-print(process_output(output_token))
-
+print(new_predicitions)
 
 
 
