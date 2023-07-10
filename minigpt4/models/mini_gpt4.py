c9294e835e0857d786417dc87e183a9999160aa0
Ximeng Sun <sunxm@Ximengs-Air.attlocal.net>
2023-07-10 16:50:39 -0700

    add minigpt4

--- minigpt4/models/mini_gpt4.py
+++ minigpt4/models/mini_gpt4.py
@@ -133,20 +133,20 @@ class MiniGPT4(Blip2Base):
         #     self.vit_to_cpu()
         #     image = image.to("cpu")
 
-        with self.maybe_autocast():
-            image_embeds = self.ln_vision(self.visual_encoder(image)).to(device)
-            image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(device)
+        # with self.maybe_autocast():
+        image_embeds = self.ln_vision(self.visual_encoder(image)).to(device)
+        image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(device)
 
-            query_tokens = self.query_tokens.expand(image_embeds.shape[0], -1, -1)
-            query_output = self.Qformer.bert(
-                query_embeds=query_tokens,
-                encoder_hidden_states=image_embeds,
-                encoder_attention_mask=image_atts,
-                return_dict=True,
-            )
+        query_tokens = self.query_tokens.expand(image_embeds.shape[0], -1, -1)
+        query_output = self.Qformer.bert(
+            query_embeds=query_tokens,
+            encoder_hidden_states=image_embeds,
+            encoder_attention_mask=image_atts,
+            return_dict=True,
+        )
 
-            inputs_llama = self.llama_proj(query_output.last_hidden_state)
-            atts_llama = torch.ones(inputs_llama.size()[:-1], dtype=torch.long).to(image.device)
+        inputs_llama = self.llama_proj(query_output.last_hidden_state)
+        atts_llama = torch.ones(inputs_llama.size()[:-1], dtype=torch.long).to(image.device)
         return inputs_llama, atts_llama
 
     def prompt_wrap(self, img_embeds, atts_img, prompt):
