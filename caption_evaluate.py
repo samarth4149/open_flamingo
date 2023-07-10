f251bb6c0ba6a6608fa3e0243fbbaeb34ddece0c
Ximeng Sun <sunxm@Ximengs-Air.attlocal.net>
2023-07-10 16:17:06 -0700

    add minigpt4

--- caption_evaluate.py
+++ caption_evaluate.py
@@ -82,6 +82,14 @@ parser.add_argument(
     default="open_flamingo",
 )
 
+parser.add_argument(
+        "--options",
+        nargs="+",
+        help="override some settings in the used config, the key-value pair "
+        "in xxx=yyy format will be merged into config file (deprecate), "
+        "change to --cfg-options instead.",
+    )
+
 parser.add_argument("--cfg-path", required=True, help="path to configuration file.")
 # parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
 
