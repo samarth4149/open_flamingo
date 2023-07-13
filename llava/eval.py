b5c9e4f1a5b189c66f8d084bfbc4b3ce67b89d78
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-07-12 17:49:46 -0700

    add llava

--- llava/eval.py
+++ llava/eval.py
@@ -60,7 +60,7 @@ class LLaVA():
 
     def get_outputs(self, batch_images, prompt):
         input_ids, stop_str, stopping_criteria = self.encode_prompt(prompt)
-
+        input_ids = input_ids.tile(batch_images, 1)
         with torch.inference_mode():
             output_ids = self.model.generate(
                 input_ids,
