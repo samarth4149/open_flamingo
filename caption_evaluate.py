36d9a2ee94f26d69236fb060c09b445737ce895e
Ximeng Sun <sunxm@Ximengs-Air.attlocal.net>
2023-07-24 13:53:59 -0700

    debug for make delta

--- caption_evaluate.py
+++ caption_evaluate.py
@@ -297,8 +297,6 @@ def evaluate_captioning(
         else:
             new_predictions = outputs
 
-        import pdb
-        pdb.set_trace()
 
         # Generate predictions from captions
         predictions = np.zeros((len(new_predictions), len(class_names)), dtype=np.int32)
