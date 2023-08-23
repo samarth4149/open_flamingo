4ff4e30cea35527520dae61221120ae0a62da53d
Ximeng Sun <sunxm@nat-wireless-guest-reg-153-51.bu.edu>
2023-08-23 14:45:01 -0400

    clip evalution

--- datasets/coco_detection.py
+++ datasets/coco_detection.py
@@ -107,7 +107,7 @@ class CocoDetection(datasets.coco.CocoDetection):
 
 if __name__ == '__main__':
     transform_clip = transforms.Compose([
-        transforms.Resize(224, interpolation=Image.BICUBIC),
+        transforms.Resize((224, 224), interpolation=Image.BICUBIC),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
     ])
