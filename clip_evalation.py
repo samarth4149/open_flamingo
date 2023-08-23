8545ad10c4f794c10edf735753e6c8bb8feece2e
Ximeng Sun <sunxm@nat-wireless-guest-reg-153-51.bu.edu>
2023-08-23 14:43:28 -0400

    clip evalution

--- clip_evalation.py
+++ clip_evalation.py
@@ -142,7 +142,7 @@ else:
 
 # define the data transform based on ELEVATER paper
 transform_clip = transforms.Compose([
-            transforms.Resize(224, interpolation=Image.BICUBIC),
+            transforms.Resize((224, 224), interpolation=Image.BICUBIC),
             transforms.ToTensor(),
             transforms.Normalize(mean= [0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
         ])
