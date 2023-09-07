9590f25b5f6e25e22d68c9dffb805486e6efbc0e
Ximeng Sun <sunxm@Ximengs-Mac-mini.local>
2023-09-07 17:01:02 -0400

    add ade20k

---
+++ tools/datasets/ade20k.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import pickle as pkl
+import utils_ade20k
+
+DATASET_PATH = '../dataset/'
+index_file = 'index_ade20k.pkl'
+with open('{}/{}'.format(DATASET_PATH, index_file), 'rb') as f:
+    index_ade20k = pkl.load(f)
+
+print("File loaded, description of the attributes:")
+print('--------------------------------------------')
+for attribute_name, desc in index_ade20k['description'].items():
+    print('* {}: {}'.format(attribute_name, desc))
+print('--------------------------------------------\n')
+
+i = 16868 # 16899, 16964
+nfiles = len(index_ade20k['filename'])
+file_name = index_ade20k['filename'][i]
+num_obj = index_ade20k['objectPresence'][:, i].sum()
+num_parts = index_ade20k['objectIsPart'][:, i].sum()
+count_obj = index_ade20k['objectPresence'][:, i].max()
+obj_id = np.where(index_ade20k['objectPresence'][:, i] == count_obj)[0][0]
+obj_name = index_ade20k['objectnames'][obj_id]
+full_file_name = '{}/{}'.format(index_ade20k['folder'][i], index_ade20k['filename'][i])
+print("The dataset has {} images".format(nfiles))
+print("The image at index {} is {}".format(i, file_name))
+print("It is located at {}".format(full_file_name))
+print("It happens in a {}".format(index_ade20k['scene'][i]))
+print("It has {} objects, of which {} are parts".format(num_obj, num_parts))
+print("The most common object is object {} ({}), which appears {} times".format(obj_name, obj_id, count_obj))
+
+root_path = DATASET_PATH
+
+# This function reads the image and mask files and generate instance and segmentation
+# masks
+info = utils_ade20k.loadAde20K('{}/{}'.format(root_path, full_file_name))
+img = cv2.imread(info['img_name'])[:,:,::-1]
+seg = cv2.imread(info['segm_name'])[:,:,::-1]
+seg_mask = seg.copy()
+
+# The 0 index in seg_mask corresponds to background (not annotated) pixels
+seg_mask[info['class_mask'] != obj_id+1] *= 0
+plt.figure(figsize=(15,5))
+
+plt.imshow(np.concatenate([img, seg, seg_mask], 1))
+plt.axis('off')
+if len(info['partclass_mask']):
+    plt.figure(figsize=(5*len(info['partclass_mask']), 5))
+    plt.title('Parts')
+    plt.imshow(np.concatenate(info['partclass_mask'],1))
+    plt.axis('off')
