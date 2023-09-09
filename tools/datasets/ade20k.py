d939458ab3438f6a0dd42fcb562d83842481bfd4
Ximeng Sun <sunxm@Ximengs-Mac-mini.local>
2023-09-09 14:25:18 -0400

    fix scripts

--- tools/datasets/ade20k.py
+++ tools/datasets/ade20k.py
@@ -1,13 +1,14 @@
 import glob
 import os
 import cv2
+import numpy as np
 
 # read in the annotation file
 annotation_folder_path = '/projectnb/ivc-ml/sunxm/datasets/ADEChallengeData2016/annotations/validation'
 lines = []
 for file in glob.glob(annotation_folder_path+'/*.png'):
     img = cv2.imread(file)
-    labels = img.unique()
+    labels = np.unique(img)
     labels_str = [str(a) for a in labels]
     base_name = os.path.basename(file)
     line = base_name + ',' + ','.join(labels_str) + '\n'
