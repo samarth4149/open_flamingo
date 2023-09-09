136df87a087e7e352b0df9639b0bc3e3095351a5
Ximeng Sun <sunxm@Ximengs-Mac-mini.local>
2023-09-09 14:30:48 -0400

    fix scripts

--- tools/datasets/ade20k.py
+++ tools/datasets/ade20k.py
@@ -12,6 +12,8 @@ for file in glob.glob(annotation_folder_path+'/*.png'):
     labels_str = [str(a) for a in labels]
     base_name = os.path.basename(file)
     line = base_name + ',' + ','.join(labels_str) + '\n'
+    import pdb
+    pdb.set_trace()
     lines.append(line)
 
 annotation_file_path = '/projectnb/ivc-ml/sunxm/datasets/ADEChallengeData2016/annotations/validation.txt'
