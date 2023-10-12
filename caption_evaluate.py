d5d08f6a08045872b3f954282126f88efe736655
Ximeng Sun <sunxm@ximengs-mini.mynetworksettings.com>
2023-10-12 16:02:22 -0400

    revise the eval

--- caption_evaluate.py
+++ caption_evaluate.py
@@ -254,7 +254,7 @@ def evaluate_captioning(
             data_split = 'val2014'
         elif dataset_name == 'pascal_voc':
             dataset_func = voc2007
-            data_split = 'val'
+            data_split = 'test'
         elif dataset_name == 'OpenImagesV6Common':
             dataset_func = OpenImagesCommon
             data_split = 'test'
