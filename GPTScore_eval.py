21fec245406e6a640e5de8ee36d79593721011e8
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-10-18 23:04:57 -0400

    add gptscore for model llama

--- GPTScore_eval.py
+++ GPTScore_eval.py
@@ -306,7 +306,6 @@ def evaluate_captioning(
 
     targets = []
     preds = []
-    count = 0
     for batch in tqdm(iter(test_dataloader)):
         batch_images, batch_target, batch_path = batch
         batch_target = batch_target.max(dim=1)[0]
@@ -333,9 +332,6 @@ def evaluate_captioning(
         # compute mAP with the ground truth label
         targets.append(batch_target)
         preds.append(outputs)
-        count += 1
-        if count > 10:
-            break
         # generate triplets for visualization
         # # Example usage:
         # visual_path = '/Users/sunxm/Documents/research/datasets/mscoco_2014/val2014'
