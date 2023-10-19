e7ad7ffba1d6cd64766eb0427b12cc95fa076504
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-10-19 00:08:53 -0400

    add gptscore for model llama

--- GPTScore_eval.py
+++ GPTScore_eval.py
@@ -328,8 +328,8 @@ def evaluate_captioning(
         targets.append(batch_target)
         preds.append(outputs)
         count += 1
-        if count>10:
-            break
+        # if count>10:
+        #     break
 
     # compute mAP with the ground truth label
     preds = torch.exp(torch.cat(preds, dim=0))
