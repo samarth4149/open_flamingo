ada20d0081ba0ebd64328f21ab10d3d0e64c1338
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-10-21 22:08:43 -0400

    add gptscore for model llama

--- datasets/image_dataset2.py
+++ datasets/image_dataset2.py
@@ -5,6 +5,9 @@ import pathlib
 from vision_datasets import Usages
 from torchvision import transforms
 from PIL import Image
+from datasets.imagenet_utils import openai_imagenet_classnames
+from datasets.elevater_utils import class_names
+
 
 VISION_DATASET_STORAGE = 'https://cvinthewildeus.blob.core.windows.net/datasets'
 
@@ -23,6 +26,10 @@ def image_dataset(dataroot, dataset_name, transform):
     test_set, test_set_dataset_info, _ = results
 
     test_set = TorchDataset(ManifestDataset(test_set_dataset_info, test_set), transform=transform)
+    if dataset_name == 'imagenet-1k':
+        test_set.classnames = openai_imagenet_classnames
+    else:
+        test_set.classnames = class_names[dataset_name]
     return test_set
 
 
@@ -33,6 +40,6 @@ if __name__ == '__main__':
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
-    dataset = image_dataset('/projectnb/ivc-ml/sunxm/datasets', 'cifar-100', transform)
+    dataset = image_dataset('/projectnb/ivc-ml/sunxm/datasets', 'imagenet-1k', transform)
     import pdb
     pdb.set_trace()
