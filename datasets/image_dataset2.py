e51b2f9c46495e70482de0e5ed1faf49fb238b5d
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-10-21 21:46:31 -0400

    add gptscore for model llama

--- datasets/image_dataset2.py
+++ datasets/image_dataset2.py
@@ -10,7 +10,7 @@ VISION_DATASET_STORAGE = 'https://cvinthewildeus.blob.core.windows.net/datasets'
 
 
 def get_dataset_hub():
-    vision_dataset_json = (pathlib.Path(__file__).resolve().parents[1] / 'vision_datasets.json').read_text()
+    vision_dataset_json = (pathlib.Path(__file__).resolve().parents[1] / 'datasets' / 'vision_datasets.json').read_text()
     hub = DatasetHub(vision_dataset_json)
 
     return hub
