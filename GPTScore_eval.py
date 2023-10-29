29e0e80ee6f44a82cab0f81ccf2a59d6b8adcd70
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-10-28 22:15:35 -0400

    add llava

--- GPTScore_eval.py
+++ GPTScore_eval.py
@@ -368,11 +368,11 @@ def evaluate_captioning(
         targets.append(batch_target)
         preds.append(outputs)
         count += 1
-    #     if count >= 1:
-    #         break
-    #
-    # import pdb
-    # pdb.set_trace()
+        if count >= 1:
+            break
+
+    import pdb
+    pdb.set_trace()
     # compute mAP with the ground truth label
     preds = torch.exp(torch.cat(preds, dim=0)).float()
     targets = torch.cat(targets, dim=0)
