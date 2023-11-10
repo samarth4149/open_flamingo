9c6f8cae3077e4147e24f889a9da3f9cbeea1fe9
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-11-10 15:32:05 -0500

    add open-clip

--- datasets/open_clip_datasets.py
+++ datasets/open_clip_datasets.py
@@ -16,7 +16,7 @@ task = get_dataset_default_task(dataset_name)
 transform = v2.Compose([
     v2.RandomResizedCrop(size=(224, 224), antialias=True),
     v2.RandomHorizontalFlip(p=0.5),
-    v2.ToDtype(torch.float32, scale=True),
+    v2.ToDtype(torch.float32),
     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 dataset = build_dataset(
